31a781afd335ef12831bf4dd2788ba9679c8d851
sheafk <sheafureyking@gmail.com>
2016-03-25 16:43:11 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,36 +1,54 @@
 def welcome
+  puts "Welcome to the Blackjack Table"
   # code #welcome here
 end
 
 def deal_card
-  # code #deal_card here
+  rand(11) + 1# code #deal_card here
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}" # code #display_card_total here
 end
 
 def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
   # code #prompt_user here
 end
 
 def get_user_input
+  answer = gets.chomp
   # code #get_user_input here
 end
 
-def end_game
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
   # code #end_game here
 end
 
 def initial_round
-  # code #initial_round here
+  card_total = deal_card + deal_card
+  display_card_total(card_total)
+  card_total# code #initial_round here
 end
 
-def hit?
+def hit?(card_total)
+  prompt_user
+  answer = get_user_input
+  if answer == 's'
+  elsif answer == 'h'
+    card_total += deal_card
+    card_total
+  else
+    invalid_comment
+    prompt_user
+  end
+card_total
   # code hit? here
 end
 
 def invalid_command
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
   # code invalid_command here
 end
 
@@ -39,6 +57,17 @@ end
 #####################################################
 
 def runner
+  welcome
+  card_total = initial_round
+  until card_total > 21 do
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+    if card_total == 21
+      break
+    end
+  end
+  end_game(card_total)
+
   # code runner here
 end
-    
+
